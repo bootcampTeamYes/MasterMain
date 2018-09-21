212c31364b91696c9f017dcc47d4b1c9538970c5
Raivitos <raivukam2@gmail.com>
2018-09-22 00:50:17 +0300

    database_easyLink.sql
    Scripts. Done and ready for work. Iekšā it kā vairs neko nevajag. Ievietotas 3 vērtības.

---
+++ bin/src/main/java/com/easyLink/easyLink/database_easyLink.sql
@@ -0,0 +1,56 @@
+
+drop database if exists easyLink;
+create database easyLink;
+use easyLink;
+
+--
+-- Pleast do not delete things you do not know about
+--
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+	SET NAMES utf8;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `easylink`
+--
+
+DROP TABLE IF EXISTS `easylink`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+CREATE TABLE `easylink` (
+  `id` varchar(2083) NOT NULL,
+  `full_url` varchar(2083) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `easylink`
+--
+
+LOCK TABLES `easylink` WRITE;
+INSERT INTO `easylink` VALUES ('lockTables','https://dev.mysql.com/doc/refman/8.0/en/lock-tables.html'),
+('primaryKey','https://www.w3schools.com/Sql/sql_primarykey.asp'),
+('mySQL','http://www.vogella.com/tutorials/MySQLJava/article.html#jdbc');
+UNLOCK TABLES;
+
+--
+-- Pleast do not delete things you do not know about
+--
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
