8043ea0e15d0858aa938cae21cf60adc0e1249fb
Tumantins <codmw8@hotmail.com>
2018-10-06 16:01:20 +0300

    about

---
+++ src/main/resources/static/indexUser.js
@@ -0,0 +1,37 @@
+$('#save-URL').click(function() {
+
+    var name = $('#input-36').val();
+
+    var data = {
+        id: $('#input-36').val(),
+        url: $('#input-35').val()
+    };
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+    if ($('#input-35').val().match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?%#[\]@!\$&'\(\)\*\+,;=.]+$/gm) == null) {
+
+        alert("Link not valid!");
+
+    } else {
+
+        $.ajax({
+            url: 'http://localhost:8080/links',
+            type: "POST",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(d) {
+
+                if (d.result) {
+                    alert("Link shortened! Find it at: localhost:8080/links/" + name);
+
+                } else {
+                    alert("Id already exists. Please choose another Id");
+                }
+            }
+        })
+
+
+    }
+});
